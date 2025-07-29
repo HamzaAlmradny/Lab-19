e4c4e8f1ab235a63b80000552978535f40fd7a25
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 21:19:04 +0300

    Add project files.

---
+++ Lab 19/Lab 19.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+     int num;
+
+     cout << "Please, enter your number: ";
+     cin >> num;
+
+     num % 2 == 0 ? cout << "EVEN"<<"\n" : cout << "ODD" << "\n";
+
+
+     return 0;
+
+
+
+
+
+
+   
+
+}
