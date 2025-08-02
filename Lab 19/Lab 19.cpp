3aa09c981b093f3683a96e06f39088ae6e47b966
HamzaAlmradny <h.almradny@gmail.com>
2025-08-02 16:30:00 +0300

    Lab 19

--- Lab 19/Lab 19.cpp
+++ Lab 19/Lab 19.cpp
@@ -1,3 +1,18 @@
+//Documentation section
+
+/*
+File Name : Lab 19
+Author : Hamza Alsayed Almradny
+Version : V.1
+Purpose : If a number is even or odd using ternary operator
+Date : 21/7/2025
+*/
+
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
